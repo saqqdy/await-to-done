4f82c6255380d9de53834e875fd0bb34a13d7d62
saqqdy <saqqdy@qq.com>
2026-03-13 16:24:02 +0800

    v1.2.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,7 +26,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:saqqdy/await-to-done.git main:gh-pages
+git push -f git@github.com:saqqdy/await-to-done.git master:gh-pages
 # git push -f git@gitee.com:saqqdy/await-to-done.git master:gh-pages
 
 cd -
